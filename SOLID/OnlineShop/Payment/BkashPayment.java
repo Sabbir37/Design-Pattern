1a23deef23227594abf49db0f175f42710d8dfcb
Sabbir Hossain <111452569+Sabbir37@users.noreply.github.com>
2023-08-13 00:37:40 +0600

    Create BkashPayment.java

---
+++ SOLID/OnlineShop/Payment/BkashPayment.java
@@ -0,0 +1,6 @@
+public class BkashPayment implements PaymentMethod {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Bkash payment processed.");
+    }
+}
