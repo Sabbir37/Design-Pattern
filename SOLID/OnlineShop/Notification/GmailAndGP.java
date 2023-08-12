05eba4d2f45317725cae21152118764581ede3d4
Sabbir Hossain <111452569+Sabbir37@users.noreply.github.com>
2023-08-13 00:09:35 +0600

    Create GmailAndGP.java

---
+++ SOLID/OnlineShop/Notification/GmailAndGP.java
@@ -0,0 +1,12 @@
+public class GmailAndGP implements EmailNotifier, SMSNotifier {
+
+    @Override
+    public void sendSMS() {
+        System.out.println("GP SMS sent.");
+    }
+
+    @Override
+    public void sendEmail() {
+        System.out.println("Gmail email sent.");
+    }
+}
