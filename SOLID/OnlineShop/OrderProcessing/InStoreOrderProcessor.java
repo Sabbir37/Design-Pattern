13fc8d9b243646330a513c51bd528fe8b1bb826d
Sabbir Hossain <111452569+Sabbir37@users.noreply.github.com>
2023-08-13 00:11:53 +0600

    Create InStoreOrderProcessor.java

---
+++ SOLID/OnlineShop/OrderProcessing/InStoreOrderProcessor.java
@@ -0,0 +1,8 @@
+public class InStoreOrderProcessor implements OrderProcessor {
+
+    @Override
+    public void processOrder(Order order) {
+        System.out.println("In-store order processed.");
+    }
+
+}
