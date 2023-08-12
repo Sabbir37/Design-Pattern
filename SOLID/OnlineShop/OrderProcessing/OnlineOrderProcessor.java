d33ff2632c082f84f4fcc28de7f1a1e543c88dad
Sabbir Hossain <111452569+Sabbir37@users.noreply.github.com>
2023-08-13 00:12:28 +0600

    Create OnlineOrderProcessor.java

---
+++ SOLID/OnlineShop/OrderProcessing/OnlineOrderProcessor.java
@@ -0,0 +1,8 @@
+public class OnlineOrderProcessor implements OrderProcessor {
+
+    @Override
+    public void processOrder(Order order) {
+        System.out.println("Online order processed.");
+    }
+
+}
