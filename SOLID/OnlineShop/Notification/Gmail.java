751f618cf860eabc594dcc9943fa1b6fd1b307d6
Sabbir Hossain <111452569+Sabbir37@users.noreply.github.com>
2023-08-13 00:08:22 +0600

    Create Gmail.java

---
+++ SOLID/OnlineShop/Notification/Gmail.java
@@ -0,0 +1,9 @@
+public class Gmail implements EmailNotifier {
+
+    @Override
+    public void sendEmail() {
+        // TODO Auto-generated method stub
+        System.out.println("Gmail email sent.");
+    }
+
+}
