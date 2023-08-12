acc92194c049689b9cb88154b7819781e99944aa
Sabbir Hossain <111452569+Sabbir37@users.noreply.github.com>
2023-08-13 00:37:16 +0600

    Create CreditCardPayment.java

---
+++ SOLID/OnlineShop/Payment/CreditCardPayment.java
@@ -0,0 +1,6 @@
+public class CreditCardPayment implements PaymentMethod {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Credit card payment processed.");
+    }
+}
